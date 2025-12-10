57f6e92608b4b98ef05835b2289a39d8ebd1c4ce
varunagrawalaxitor <varun.agrawal@aixtor.com>
2025-12-10 17:36:04 +0530

    configuration

--- meetup/modules/meetup-common-rest/meetup-common-rest-impl/src/main/java/com/meetup/common/rest/internal/resource/v1_0_0/ServiceEntityResourceImpl.java
+++ meetup/modules/meetup-common-rest/meetup-common-rest-impl/src/main/java/com/meetup/common/rest/internal/resource/v1_0_0/ServiceEntityResourceImpl.java
@@ -1,5 +1,6 @@
 package com.meetup.common.rest.internal.resource.v1_0_0;
 
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.util.Base64;
 import com.meetup.common.rest.internal.util.CommonRestUtil;
 import com.meetup.common.rest.resource.v1_0_0.ServiceEntityResource;
@@ -9,6 +10,7 @@ import javax.validation.constraints.NotNull;
 import javax.ws.rs.core.Context;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -22,6 +24,9 @@ public class ServiceEntityResourceImpl extends BaseServiceEntityResourceImpl {
 	
 	 @Context
 	  private HttpServletRequest httpServletRequest;
+	 
+	 @Reference
+     private ConfigurationProvider configurationProvider ;
 
 	@Override
 	public Object getEntityListNew(@NotNull String propsDataEnc, @NotNull Integer userId, @NotNull Integer siteId)
@@ -29,7 +34,7 @@ public class ServiceEntityResourceImpl extends BaseServiceEntityResourceImpl {
 		 
 		byte[] propsDataBytes = Base64.decode(propsDataEnc);
 		String propsData = new String(propsDataBytes);
-		
+		 CommonRestUtil.initialize(configurationProvider);
 		return CommonRestUtil.getEntityList(propsData, userId, siteId, httpServletRequest);
   
 	}
